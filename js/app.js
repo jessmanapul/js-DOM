f1104d55b7d3f20b627f18c232a8afa6680af425
Jess Manapul <Jess@Jess.local>
2019-02-20 16:41:18 -1000

    completed assignment

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ Find the div with the id of "name1" and replace the n/a with the following:
 
 Tay-Tay*/
 
+var taylor = document.getElementById('name1');
+taylor.innerHTML = 'Tay-Tay';
+
 
 /*2. DJ Khaled
 
@@ -13,6 +16,8 @@ Find the div with the id of "position2" and replace the n/a with the following:
 
 Project Manager*/
 
+var khaled = document.getElementById('position2');
+position2.innerHTML = 'Project Manager';
 
 /*3. Piko Taro
 
@@ -20,24 +25,29 @@ Find the div with the id of "alias3" and replace the n/a with the following:
 
 Concatenation*/
 
+var piko = document.getElementById('alias3');
+piko.innerHTML = 'Concatenation';
 
 /*4. Prince
 
 Find the div with the class name of "profile" and replace the n/a with a verse from your favorite Prince song.*/
 
-
+var prince = document.getElementsByClassName('profile');
+prince[0].innerHTML = 'Never meant to cause you any sorrow';
 
 /*5. Bruce Lee
 
 Find the div with the class name of "profile" and replace the n/a with a quote from the legend himself.*/
 
-
+var bruce = document.getElementsByClassName('profile');
+bruce[1].innerHTML = 'Wata';
 
 /*6. Samuel L Jackson
 
 Find the div with the class name of "alias" and replace the n/a with your favorite character that Sammy portrayed.*/
 
-
+var samuel = document.getElementsByClassName('alias');
+samuel[2].innerHTML  = 'Samuel Jackson';
 
 /*7. David Ige
 
@@ -45,15 +55,26 @@ Create a div element and give it an id of "name7". Inside this div element, give
 
 Append this div element to the nameParent div*/
 
- 
+var nameElem =  document.createElement('div');
+nameElem.id = 'name7';
+nameElem.innerHTML = 'David Ige';
+nameParent.appendChild(nameElem);
+
 /*8. The Dos Equis Dude
 
 Create a div element give it an id of "alias8". Inside this div element, give it the contents of "Stay Thirsty My Friends".
 
 Append this div element to the aliasParent div.*/
 
-
+var aliasElem = document.createElement('div');
+aliasElem.id = 'alias8';
+aliasElem.innerHTML = 'Stay Thirsty My Friends';
+aliasParent.appendChild(aliasElem);
 
 //Final Boss
 /*9. Create your own profile.*/
 
+// var myName = document.createElement('div');
+// myName.id = 'name9';
+// myName.innerHTML = "Jess";
+// nameParent.appendChild(myName);
